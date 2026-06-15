8d1bd8c4d697fa8e26d7defaaf0743aac3bdf17f
Jan Gustavo <jandersongustavo@example.com>
2026-06-15 17:10:50 -0300

    chore: altera MainActivity para estender FlutterFragmentActivity

--- android/app/src/main/kotlin/dev/jangustavo/metemarcha/MainActivity.kt
+++ android/app/src/main/kotlin/dev/jangustavo/metemarcha/MainActivity.kt
@@ -1,11 +1,11 @@
 package dev.jangustavo.metemarcha
 
 import android.content.Intent
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val CHANNEL = "dev.jangustavo.metemarcha/app_launcher"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
